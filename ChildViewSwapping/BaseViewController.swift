53d677c6cea683a6aa43e2f82af6541639cc073e
prakashmaharjan <prakashmaharjan20@gmail.com>
2020-09-04 20:02:29 +0545

    button customisation

--- ChildViewSwapping/BaseViewController.swift
+++ ChildViewSwapping/BaseViewController.swift
@@ -9,29 +9,47 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-
+    
     @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var view1Button: UIButton!
+    @IBOutlet weak var view2Button: UIButton!
+    @IBOutlet weak var view3Button: UIButton!
+    
     var index = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.title = "Child View Demo"
+        self.view1Button.setTitleColor(.red, for: .normal)
+        self.view2Button.setTitleColor(.lightGray, for: .normal)
+        self.view3Button.setTitleColor(.lightGray, for: .normal)
         setupView()
-
+        
     }
-
+    
     @IBAction func view1Tapped(_ sender: AnyObject) {
         self.index = 0
+        self.view1Button.setTitleColor(.red, for: .normal)
+        self.view2Button.setTitleColor(.lightGray, for: .normal)
+        self.view3Button.setTitleColor(.lightGray, for: .normal)
         self.setupView()
     }
     
     @IBAction func view2Tapped(_ sender: AnyObject) {
         self.index = 1
+        self.view1Button.setTitleColor(.lightGray, for: .normal)
+        self.view2Button.setTitleColor(.green, for: .normal)
+        self.view3Button.setTitleColor(.lightGray, for: .normal)
         self.setupView()
     }
- 
+    
     
     @IBAction func view3Tapped(_ sender: AnyObject) {
         self.index = 2
+        self.view1Button.setTitleColor(.lightGray, for: .normal)
+        self.view2Button.setTitleColor(.lightGray, for: .normal)
+        self.view3Button.setTitleColor(.blue, for: .normal)
         self.setupView()
     }
     
@@ -39,11 +57,11 @@ class BaseViewController: UIViewController {
     // MARK: - View Methods
     
     fileprivate func setupView() {
-
+        
         
         updateView()
     }
-
+    
     fileprivate func updateView() {
         if index == 0 {
             remove(asChildViewController: secondViewController)
@@ -66,7 +84,7 @@ class BaseViewController: UIViewController {
         
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "ViewController1") as! ViewController1
-       //viewController.view.frame = CGRectMake(0, 135, 375 , 500)
+        //viewController.view.frame = CGRectMake(0, 135, 375 , 500)
         // Add View Controller as Child View Controller
         self.add(asChildViewController: viewController)
         
@@ -74,13 +92,13 @@ class BaseViewController: UIViewController {
         
         
     }()
-
+    
     fileprivate lazy var secondViewController: ViewController2 = {
-   
+        
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-
+        
         let viewController = storyboard.instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
-       //viewController.view.frame = CGRectMake(0, 135, 375 , 500)
+        //viewController.view.frame = CGRectMake(0, 135, 375 , 500)
         // Add View Controller as Child View Controller
         self.add(asChildViewController: viewController)
         
@@ -93,9 +111,9 @@ class BaseViewController: UIViewController {
         //let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-
+        
         let viewController = storyboard.instantiateViewController(withIdentifier: "ViewController3") as! ViewController3
-          //viewController.view.frame = CGRectMake(0, 135, 375 , 500)
+        //viewController.view.frame = CGRectMake(0, 135, 375 , 500)
         
         // Add View Controller as Child View Controller
         self.add(asChildViewController: viewController)
@@ -111,19 +129,19 @@ class BaseViewController: UIViewController {
         view.addSubview(viewController.view)
         
         // Configure Child View
-       // viewController.view.frame = view.bounds
+        // viewController.view.frame = view.bounds
         viewController.view.frame = CGRect(x: 0, y: 150, width: self.view.frame.size.width, height: self.view.frame.size.height - 150)
-     
+        
         viewController.view.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
         
         // Notify Child View Controller
-
+        
         viewController.didMove(toParent: self)
     }
     
     fileprivate func remove(asChildViewController viewController: UIViewController) {
         // Notify Child View Controller
- 
+        
         viewController.willMove(toParent: nil)
         
         // Remove Child View From Superview
@@ -132,12 +150,12 @@ class BaseViewController: UIViewController {
         // Notify Child View Controller
         viewController.removeFromParent()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
